d1f4225e409f61ad00d681dcaf448815b141549a
Jakaria Hossain <70642638+Jaki-pro@users.noreply.github.com>
2024-10-07 10:21:54 +0600

    Create Next permutation

---
+++ Next permutation.cpp
@@ -0,0 +1,11 @@
+    n = 4; // n can be upto 9
+    ll a[n] = {1, 2, 3, 4}; 
+    do
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout<<a[i]<<' ';
+        }
+        cout<<"\n";
+    }
+    while (next_permutation(a, a + n));
