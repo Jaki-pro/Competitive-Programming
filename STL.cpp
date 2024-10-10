efa6ef7c1746a09273ac616c65163ff432f9f6af
Jakaria Hossain <70642638+Jaki-pro@users.noreply.github.com>
2024-10-10 19:30:09 +0600

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -1,1 +1,15 @@
+// Priority queue
 priority_queue <int, vector<int>, greater<int>> pq;
+
+// Next permuation
+ll n= 4; // n can be upto 9
+ll a[n] = {1, 2, 3, 4}; 
+do
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<' ';
+    }
+    cout<<"\n";
+}
+while (next_permutation(a, a + n));
