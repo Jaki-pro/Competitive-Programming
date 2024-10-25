2166a23aa9138c13d7cfddf43fea0790b5aeb080
Jakaria Hossain <70642638+Jaki-pro@users.noreply.github.com>
2024-10-25 10:38:50 +0600

    Create BigMod.cpp

---
+++ Number Theory/BigMod.cpp
@@ -0,0 +1,9 @@
+int power(long long base, long long k, const int mod) {
+  int ans = 1 % mod;
+  base %= mod;
+  if (base < 0) base += mod;
+  while (k) {
+    if (k & 1) ans = (long long) ans * base % mod;
+    base = (long long) base * base % mod;
+    k >>= 1;
+  }
